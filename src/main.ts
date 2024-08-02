ed41a225b08a1e36a6c9eb62ad7ba0570ce9d0a7
Matt King <mattkingit@gmail.com>
2024-08-02 16:32:16 -0500

    On Demand Rendering

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,24 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement
-const renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.setSize(200, 200)
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.render(scene, camera) // render whenever the screen size changes
+})
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', () => {
+  renderer.render(scene, camera) // render whenever the OrbitControls changes
+})
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
@@ -19,10 +29,4 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-function animate() {
-  requestAnimationFrame(animate)
-
-  renderer.render(scene, camera)
-}
-
-animate()
+renderer.render(scene, camera) // render once when the scene has been setup
